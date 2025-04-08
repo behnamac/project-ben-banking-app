e58b26c0f5d6ade3129753d2b5c126df45918524
Behnam <behnam.sep@gmail.com>
2025-04-08 15:19:00 +0200

    Refactor BankCard component to display account name and balance; enhance utils with date formatting and amount handling functions

--- app/components/BankCard.tsx
+++ app/components/BankCard.tsx
@@ -1,13 +1,27 @@
+import Link from "next/link";
 import React from "react";
-
+import { formatAmount } from "@/lib/utils";
 interface BankCardProps {
   account: any;
   userName: string;
   showBalance?: boolean;
 }
 
-const BankCard = ({ account, userName, showBalance }: BankCardProps) => {
-  return <div>BankCard</div>;
+const BankCard = ({ account, userName, showBalance = true }: BankCardProps) => {
+  return (
+    <div className="flex flex-col">
+      <Link href="/" className="bank-card">
+        <div className="bank-card_content">
+          <div>
+            <h1 className="text-16 font-semibold text-white">
+              {account.name || userName}
+            </h1>
+            <p className="font-ibm-plex-serif font-black text-white">{formatAmount(account.currentBalance)}</p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default BankCard;
