1f41542ba9e705c786494d6df37a41b6a85ef36a
Behnam Sepehri <behnam.sep@gmail.com>
2025-05-26 15:44:14 +0200

    Add BankCard component and update RightSidebar to display bank information

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import BankCard from "./BankCard";
 
 interface RightSidebarProps {
   user: { firstName: string; lastName: string; email: string };
@@ -35,6 +36,28 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
             <h2 className="text-14 font-semibold text-gray-600">Add Bank</h2>
           </Link>
         </div>
+        {banks.length > 0 && (
+          <div className="relative flex flex-col flex-1 items-center justify-center gap-5">
+            <div className="relative z-10">
+              <BankCard
+                key={banks[0].$id}
+                account={banks[0]}
+                userName={`${user.firstName} ${user.lastName}`}
+                showBalance={false}
+              />
+            </div>
+            {banks[1] && (
+              <div className="absolute right-0 top-8 z-0 w-[90%]">
+                <BankCard
+                  key={banks[1].$id}
+                  account={banks[1]}
+                  userName={`${user.firstName} ${user.lastName}`}
+                  showBalance={false}
+                />{" "}
+              </div>
+            )}
+          </div>
+        )}
       </section>
     </aside>
   );
